c9ada18ace434b1b2dfcb15031eece3a3c5d323b
Pedro A. Jimenez <jimenez@ucar.edu>
2025-10-24 14:37:19 -0600

    Formatting edits with no impact

--- share/mpi_mod.F90
+++ share/mpi_mod.F90
@@ -99,71 +99,65 @@
 #ifdef DM_PARALLEL
       call MPI_Comm_rank(cart_comm, rank, ierr)
       call MPI_Cart_coords(cart_comm, rank, 2, coords, ierr)
-!      print *, 'Rank =', rank, 'Coords =', coords
 
       nx = ipe - ips + 1
       ny = jpe - jps + 1
 
       tag_base = 1000
 
-        ! Diagnostics
-!      print *, 'rank =', rank, 'before = ', patch(ips-nghost:ipe+nghost, jps)
-!      print *, 'rank =', rank, 'antes  = ', patch(ips, jps-nghost:jpe+nghost)
-
         ! Get neighbor ranks in Cartesian topology
       call MPI_Cart_shift(cart_comm, 0, 1, nbr_left, nbr_right, ierr)
       call MPI_Cart_shift(cart_comm, 1, 1, nbr_down, nbr_up, ierr)
-!      print *, 'Rank=', rank, 'Nbr_up=', nbr_up, 'Nbr_down=', nbr_down
 
         ! Allocate buffers
-      allocate(sendbuf_right(ny*nghost), recvbuf_left(ny*nghost))
-      allocate(sendbuf_left(ny*nghost),  recvbuf_right(ny*nghost))
-      allocate(sendbuf_up(nx*nghost),    recvbuf_down(nx*nghost))
-      allocate(sendbuf_down(nx*nghost),  recvbuf_up(nx*nghost))
+      allocate (sendbuf_right(ny * nghost), recvbuf_left(ny * nghost))
+      allocate (sendbuf_left(ny * nghost), recvbuf_right(ny * nghost))
+      allocate (sendbuf_up(nx * nghost), recvbuf_down(nx * nghost))
+      allocate (sendbuf_down(nx * nghost), recvbuf_up(nx * nghost))
 
         ! Send RIGHT, receive LEFT
       k = 0
       do j = jps, jpe
-        do i = ipe-nghost+1, ipe
+        do i = ipe - nghost + 1, ipe
           k = k + 1
-          sendbuf_right(k) = patch(i,j)
+          sendbuf_right(k) = patch(i, j)
         end do
       end do
-      call MPI_Irecv(recvbuf_left,  ny*nghost, MPI_REAL, nbr_left,  tag_base+0, cart_comm, reqs(1), ierr)
-      call MPI_Isend(sendbuf_right, ny*nghost, MPI_REAL, nbr_right, tag_base+0, cart_comm, reqs(2), ierr)
+      call MPI_Irecv(recvbuf_left, ny*nghost, MPI_REAL, nbr_left, tag_base + 0, cart_comm, reqs(1), ierr)
+      call MPI_Isend(sendbuf_right, ny*nghost, MPI_REAL, nbr_right, tag_base + 0, cart_comm, reqs(2), ierr)
 
       ! Send LEFT, receive RIGHT
       k = 0
       do j = jps, jpe
-        do i = ips, ips+nghost-1
+        do i = ips, ips + nghost - 1
           k = k + 1
-          sendbuf_left(k) = patch(i,j)
+          sendbuf_left(k) = patch(i, j)
         end do
       end do
-      call MPI_Irecv(recvbuf_right, ny*nghost, MPI_REAL, nbr_right, tag_base+1, cart_comm, reqs(3), ierr)
-      call MPI_Isend(sendbuf_left,  ny*nghost, MPI_REAL, nbr_left,  tag_base+1, cart_comm, reqs(4), ierr)
+      call MPI_Irecv (recvbuf_right, ny * nghost, MPI_REAL, nbr_right, tag_base + 1, cart_comm, reqs(3), ierr)
+      call MPI_Isend (sendbuf_left, ny * nghost, MPI_REAL, nbr_left, tag_base + 1, cart_comm, reqs(4), ierr)
 
         ! Send UP, receive DOWN
       k = 0
-      do j = jpe-nghost+1, jpe
+      do j = jpe - nghost + 1, jpe
         do i = ips, ipe
           k = k + 1
-          sendbuf_up(k) = patch(i,j)
+          sendbuf_up(k) = patch(i, j)
         end do
       end do
-      call MPI_Irecv(recvbuf_down, nx*nghost, MPI_REAL, nbr_down, tag_base+2, cart_comm, reqs(5), ierr)
-      call MPI_Isend(sendbuf_up,   nx*nghost, MPI_REAL, nbr_up,   tag_base+2, cart_comm, reqs(6), ierr)
+      call MPI_Irecv (recvbuf_down, nx * nghost, MPI_REAL, nbr_down, tag_base + 2, cart_comm, reqs(5), ierr)
+      call MPI_Isend (sendbuf_up, nx * nghost, MPI_REAL, nbr_up, tag_base + 2, cart_comm, reqs(6), ierr)
 
       ! Send DOWN, receive UP
       k = 0
-      do j = jps, jps+nghost-1
+      do j = jps, jps + nghost - 1
         do i = ips, ipe
           k = k + 1
-          sendbuf_down(k) = patch(i,j)
+          sendbuf_down(k) = patch(i, j)
         end do
       end do
-      call MPI_Irecv(recvbuf_up,   nx*nghost, MPI_REAL, nbr_up,   tag_base+3, cart_comm, reqs(7), ierr)
-      call MPI_Isend(sendbuf_down, nx*nghost, MPI_REAL, nbr_down, tag_base+3, cart_comm, reqs(8), ierr)
+      call MPI_Irecv(recvbuf_up, nx * nghost, MPI_REAL, nbr_up, tag_base + 3, cart_comm, reqs(7), ierr)
+      call MPI_Isend(sendbuf_down, nx * nghost, MPI_REAL, nbr_down, tag_base + 3, cart_comm, reqs(8), ierr)
 
        ! Wait for all communications
       call MPI_Waitall(8, reqs, MPI_STATUSES_IGNORE, ierr)
@@ -173,9 +167,9 @@
       if (nbr_left /= MPI_PROC_NULL) then
         k = 0
         do j = jps, jpe
-          do i = ips-nghost, ips-1
+          do i = ips - nghost, ips - 1
             k = k + 1
-            patch(i,j) = recvbuf_left(k)
+            patch(i, j) = recvbuf_left(k)
           end do
         end do
       end if
@@ -184,44 +178,40 @@
       if (nbr_right /= MPI_PROC_NULL) then
         k = 0
         do j = jps, jpe
-          do i = ipe+1, ipe+nghost
+          do i = ipe + 1, ipe + nghost
             k = k + 1
-            patch(i,j) = recvbuf_right(k)
+            patch(i, j) = recvbuf_right(k)
           end do
         end do
       end if
 
-      ! UP ghost (top halo rows)
+        ! UP ghost (top halo rows)
       if (nbr_up /= MPI_PROC_NULL) then
         k = 0
-        do j = jpe+1, jpe+nghost
+        do j = jpe + 1, jpe + nghost
           do i = ips, ipe
             k = k + 1
-            patch(i,j) = recvbuf_up(k)
+            patch(i, j) = recvbuf_up(k)
           end do
         end do
       end if
 
-      ! DOWN ghost (bottom halo rows)
+        ! DOWN ghost (bottom halo rows)
       if (nbr_down /= MPI_PROC_NULL) then
         k = 0
-        do j = jps-nghost, jps-1
+        do j = jps - nghost, jps - 1
           do i = ips, ipe
             k = k + 1
-            patch(i,j) = recvbuf_down(k)
+            patch(i, j) = recvbuf_down(k)
           end do
         end do
       end if
 
-        ! Diagnostics
-!      print *, 'rank =', rank, 'after = ', patch(ips-nghost:ipe+nghost, jps)
-!      print *, 'rank =', rank, 'despues = ', patch(ips, jps-nghost:jpe+nghost)
-
         ! Deallocate buffers
-      deallocate(sendbuf_right, recvbuf_left)
-      deallocate(sendbuf_left,  recvbuf_right)
-      deallocate(sendbuf_up,    recvbuf_down)
-      deallocate(sendbuf_down,  recvbuf_up)
+      deallocate (sendbuf_right, recvbuf_left)
+      deallocate (sendbuf_left, recvbuf_right)
+      deallocate (sendbuf_up, recvbuf_down)
+      deallocate (sendbuf_down, recvbuf_up)
 
 #endif
 
