339fa122f3a791780eab0aa4cd7a1ce27bc85c56
Pedro A. Jimenez <jimenez@ucar.edu>
2026-01-30 17:54:38 -0700

    Better initialization of settings in the fire block

--- io/namelist_mod.F90
+++ io/namelist_mod.F90
@@ -402,81 +402,32 @@
       class (namelist_t), intent (in out) :: this
       character (len = *), intent (in) :: file_name
 
-      integer :: fire_print_msg = 0           ! "write fire statistics, 0 no writes, 1+ for more"  ""
-      real :: fire_atm_feedback = 1.0         ! "the heat fluxes to the atmosphere are multiplied by this" "1"
-      integer :: fire_upwinding = 9           ! "upwind normal spread: 1=standard, 2=godunov, 3=eno, 4=sethian, 5=2nd-order, 6=WENO3, 7=WENO5, 8=hybrid WENO3/ENO1, 9=hybrid WENO5/ENO1" "1"
-      real :: fire_viscosity = 0.4            ! "artificial viscosity in level set method" "1"
-      logical :: fire_lsm_reinit = .true.     ! "flag to activate reinitialization of level set method"
-      integer :: fire_lsm_reinit_iter = 1     ! "number of iterations for the reinitialization PDE"
-      integer :: fire_upwinding_reinit = 4    ! "numerical scheme (space) for reinitialization PDE: 1=WENO3, 2=WENO5, 3=hybrid WENO3-ENO1, 4=hybrid WENO5-ENO1"
-      integer :: fire_lsm_band_ngp = 4        ! "number of grid points around lfn=0 that WENO5/3 is used (ENO1 elsewhere), for fire_upwinding_reinit=4,5 and fire_upwinding=8,9 options"
-      integer :: fast_dist_reinit_opt = 0     ! Fast distance reinit method: 0) None, 1) FSM
-      integer :: fast_dist_reinit_freq = 600  ! Number of time steps to perform a reinit with fast distance reinit method
-      logical :: fire_lsm_zcoupling = .false. ! "flag to activate reference velocity at a different height from fire_wind_height"
-      real :: fire_lsm_zcoupling_ref = 50.0   ! "reference height from wich u at fire_wind_hegiht is calculated using a logarithmic profile" "m"
-      real :: fire_viscosity_bg = 0.4         ! "artificial viscosity in the near-front region" "1"
-      real :: fire_viscosity_band = 0.5       ! "number of times the hybrid advection band to transition from fire_viscosity_bg to fire_viscosity" "1"
-      integer :: fire_viscosity_ngp = 2       ! "number of grid points around lfn=0 where low artificial viscosity is used = fire_viscosity_bg"
-      logical :: fmoist_run = .false.         ! "run moisture model (on the atmospheric grid), output to fmc_gc"
-      integer :: fmoist_freq = 0              ! "frequency to run moisture model 0: use fmoist_dt, k>0: every k timesteps" "1"
-      real :: fmoist_dt = 600                 ! "moisture model time step" "s"
-      real :: fire_wind_height = 6.096        ! "height of uah,vah wind in fire spread formula" "m"
-      integer :: wind_vinterp_opt = 1         ! "mid-flame height wind interpolation option: 0) Interp to specified height, 1) Use WAFs"
-      integer :: hinterp_opt = 2              ! "Horizontal interpolation from atm to fire (offline option): 1) ngp, 2) bi-linear"
-      logical :: fire_is_real_perim = .false. ! .false. = point/line ignition, .true. = observed perimeter"
-      real :: frac_fburnt_to_smoke = 0.02     ! "parts per unit of burned fuel becoming smoke " "g_smoke/kg_air"
-      real :: fuelmc_g = 0.08                 ! Fuel moisture content ground (Dead FMC)
-      real :: fuelmc_g_live = 0.30            ! Fuel moisture content ground (Live FMC). 30% Completely cured, treat as dead fuel
-      real :: fuelmc_c = 1.00                 ! Fuel moisture content canopy
-
-      integer :: ideal_opt = 0
-
-        ! Objects
-      integer :: fuel_opt = 1 ! Fuel model
-      integer :: ros_opt = 0 ! ROS parameterization
-      integer :: fmc_opt = -1 ! FMC model
-      integer :: emis_opt = 0 ! Smoke emissions
+      integer :: fire_print_msg, fire_upwinding, fire_lsm_reinit_iter, fire_upwinding_reinit, fire_lsm_band_ngp, &
+          fast_dist_reinit_opt, fast_dist_reinit_freq, fire_viscosity_ngp, wind_vinterp_opt, hinterp_opt, ideal_opt, &
+          fuel_opt, ros_opt, fmc_opt, emis_opt, fmoist_freq
+      real :: fire_atm_feedback, fire_viscosity, fire_lsm_zcoupling_ref, fire_viscosity_bg, fire_viscosity_band, &
+          fmoist_dt, fire_wind_height, frac_fburnt_to_smoke, fuelmc_g, fuelmc_g_live, fuelmc_c
+      logical :: fire_lsm_reinit, fire_lsm_zcoupling, fmoist_run, fire_is_real_perim
 
         ! ignitions
-      integer :: fire_num_ignitions = 0
+      integer :: fire_num_ignitions
+      real :: fire_ignition_start_lon1, fire_ignition_start_lat1, fire_ignition_end_lon1, fire_ignition_end_lat1, &
+          fire_ignition_ros1, fire_ignition_start_time1, fire_ignition_end_time1, fire_ignition_radius1
+      real :: fire_ignition_start_lon2, fire_ignition_start_lat2, fire_ignition_end_lon2, fire_ignition_end_lat2, &
+          fire_ignition_ros2, fire_ignition_start_time2, fire_ignition_end_time2, fire_ignition_radius2
+      real :: fire_ignition_start_lon3, fire_ignition_start_lat3, fire_ignition_end_lon3, fire_ignition_end_lat3, &
+          fire_ignition_ros3, fire_ignition_start_time3, fire_ignition_end_time3, fire_ignition_radius3
+      real :: fire_ignition_start_lon4, fire_ignition_start_lat4, fire_ignition_end_lon4, fire_ignition_end_lat4, &
+          fire_ignition_ros4, fire_ignition_start_time4, fire_ignition_end_time4, fire_ignition_radius4
+      real :: fire_ignition_start_lon5, fire_ignition_start_lat5, fire_ignition_end_lon5, fire_ignition_end_lat5, &
+          fire_ignition_ros5, fire_ignition_start_time5, fire_ignition_end_time5, fire_ignition_radius5
 
-      real :: fire_ignition_start_lon1 = 0.0  ! "long coord of start of ignition line" "deg"
-      real :: fire_ignition_start_lat1 = 0.0  ! "lat coord of start of ignition line" "deg"
-      real :: fire_ignition_end_lon1 = 0.0    ! "long coord of end of ignition line" "deg"
-      real :: fire_ignition_end_lat1 = 0.0    ! "lat coord of end of ignition line" "deg"
-      real :: fire_ignition_ros1 = 0.01       ! "rate of spread during ignition" "m/s"
-      real :: fire_ignition_start_time1 = 0.0 ! "ignition line start time" "s"
-      real :: fire_ignition_end_time1 = 0.0   ! "ignition line end time" "s"
-      real :: fire_ignition_radius1 = 0.0     ! "ignite all within the radius" "m"
-
-      real :: fire_ignition_start_lon2 = 0.0, fire_ignition_start_lat2 = 0.0, fire_ignition_end_lon2 = 0.0, &
-          fire_ignition_end_lat2 = 0.0, &
-          fire_ignition_ros2 = 0.01, fire_ignition_start_time2 = 0.0, fire_ignition_end_time2 = 0.0, fire_ignition_radius2 = 0.0
-
-      real :: fire_ignition_start_lon3 = 0.0, fire_ignition_start_lat3 = 0.0, fire_ignition_end_lon3 = 0.0, &
-          fire_ignition_end_lat3 = 0.0, &
-          fire_ignition_ros3 = 0.01, fire_ignition_start_time3 = 0.0, fire_ignition_end_time3 = 0.0, fire_ignition_radius3 = 0.0
-
-      real :: fire_ignition_start_lon4 = 0.0, fire_ignition_start_lat4 = 0.0, fire_ignition_end_lon4 = 0.0, &
-          fire_ignition_end_lat4 = 0.0, &
-          fire_ignition_ros4 = 0.01, fire_ignition_start_time4 = 0.0, fire_ignition_end_time4 = 0.0, fire_ignition_radius4 = 0.0
-
-      real :: fire_ignition_start_lon5 = 0.0, fire_ignition_start_lat5 = 0.0, fire_ignition_end_lon5 = 0.0, &
-          fire_ignition_end_lat5 = 0.0, &
-          fire_ignition_ros5 = 0.01, fire_ignition_start_time5 = 0.0, fire_ignition_end_time5 = 0.0, fire_ignition_radius5 = 0.0
-
-      namelist /fire/  fire_print_msg, fire_atm_feedback, &
-          fire_upwinding, fire_viscosity, fire_lsm_reinit, fast_dist_reinit_opt, fast_dist_reinit_freq, &
-          fire_lsm_reinit_iter, fire_upwinding_reinit, fire_lsm_band_ngp, fire_lsm_zcoupling, fire_lsm_zcoupling_ref, &
-          fire_viscosity_bg, fire_viscosity_band, fire_viscosity_ngp, fmoist_run, &
-          fmoist_freq, fmoist_dt, &
-          fire_wind_height, fire_is_real_perim, frac_fburnt_to_smoke, fuelmc_g, &
-          fuelmc_g_live, fuelmc_c, &
-          ideal_opt, &
-            ! objects
-          fuel_opt, ros_opt, fmc_opt, emis_opt, &
-          wind_vinterp_opt, &
-          hinterp_opt, &
+      namelist /fire/  fire_print_msg, fire_atm_feedback, fire_upwinding, fire_viscosity, fire_lsm_reinit, &
+          fast_dist_reinit_opt, fast_dist_reinit_freq, fire_lsm_reinit_iter, fire_upwinding_reinit, &
+          fire_lsm_band_ngp, fire_lsm_zcoupling, fire_lsm_zcoupling_ref, fire_viscosity_bg, fire_viscosity_band, &
+          fire_viscosity_ngp, fmoist_run, fmoist_freq, fmoist_dt, fire_wind_height, fire_is_real_perim, &
+          frac_fburnt_to_smoke, fuelmc_g, fuelmc_g_live, fuelmc_c, ideal_opt, fuel_opt, ros_opt, fmc_opt, emis_opt, &
+          wind_vinterp_opt, hinterp_opt, &
             ! Ignitions
           fire_num_ignitions, &
             ! Ignition 1
@@ -498,6 +449,87 @@
       integer :: unit_nml, io_stat
       character (len = :), allocatable :: msg
 
+
+      fire_print_msg = this%fire_print_msg
+      fire_atm_feedback = this%fire_atm_feedback
+      fire_upwinding = this%fire_upwinding
+      fire_viscosity = this%fire_viscosity
+      fire_lsm_reinit = this%fire_lsm_reinit
+      fire_lsm_reinit_iter = this%fire_lsm_reinit_iter
+      fire_upwinding_reinit = this%fire_upwinding_reinit
+      fire_lsm_band_ngp = this%fire_lsm_band_ngp
+      fast_dist_reinit_opt = this%fast_dist_reinit_opt
+      fast_dist_reinit_freq = this%fast_dist_reinit_freq
+      fire_lsm_zcoupling = this%fire_lsm_zcoupling
+      fire_lsm_zcoupling_ref = this%fire_lsm_zcoupling_ref
+      fire_viscosity_bg = this%fire_viscosity_bg
+      fire_viscosity_band = this%fire_viscosity_band
+      fire_viscosity_ngp = this%fire_viscosity_ngp
+      fmoist_run = this%fmoist_run
+      fmoist_freq = this%fmoist_freq
+      fmoist_dt = this%fmoist_dt
+      fire_wind_height = this%fire_wind_height
+      fire_is_real_perim = this%fire_is_real_perim
+      frac_fburnt_to_smoke = this%frac_fburnt_to_smoke
+      fuelmc_g = this%fuelmc_g
+      fuelmc_g_live = this%fuelmc_g_live
+      fuelmc_c = this%fuelmc_c
+
+      ideal_opt = this%ideal_opt
+
+      fuel_opt = this%fuel_opt
+      ros_opt = this%ros_opt
+      fmc_opt = this%fmc_opt
+      emis_opt = this%emis_opt
+      wind_vinterp_opt = this%wind_vinterp_opt
+      hinterp_opt = this%hinterp_opt
+
+      fire_num_ignitions = this%fire_num_ignitions
+
+      fire_ignition_start_lon1 = this%fire_ignition_start_lon1
+      fire_ignition_start_lat1 = this%fire_ignition_start_lat1
+      fire_ignition_end_lon1 = this%fire_ignition_end_lon1
+      fire_ignition_end_lat1 = this%fire_ignition_end_lat1
+      fire_ignition_ros1 = this%fire_ignition_ros1
+      fire_ignition_start_time1 = this%fire_ignition_start_time1
+      fire_ignition_end_time1 = this%fire_ignition_end_time1
+      fire_ignition_radius1 = this%fire_ignition_radius1
+
+      fire_ignition_start_lon2 = this%fire_ignition_start_lon2
+      fire_ignition_start_lat2 = this%fire_ignition_start_lat2
+      fire_ignition_end_lon2 = this%fire_ignition_end_lon2
+      fire_ignition_end_lat2 = this%fire_ignition_end_lat2
+      fire_ignition_ros2 = this%fire_ignition_ros2
+      fire_ignition_start_time2 = this%fire_ignition_start_time2
+      fire_ignition_end_time2 = this%fire_ignition_end_time2
+      fire_ignition_radius2 = this%fire_ignition_radius2
+
+      fire_ignition_start_lon3 = this%fire_ignition_start_lon3
+      fire_ignition_start_lat3 = this%fire_ignition_start_lat3
+      fire_ignition_end_lon3 = this%fire_ignition_end_lon3
+      fire_ignition_end_lat3 = this%fire_ignition_end_lat3
+      fire_ignition_ros3 = this%fire_ignition_ros3
+      fire_ignition_start_time3 = this%fire_ignition_start_time3
+      fire_ignition_end_time3 = this%fire_ignition_end_time3
+      fire_ignition_radius3 = this%fire_ignition_radius3
+
+      fire_ignition_start_lon4 = this%fire_ignition_start_lon4
+      fire_ignition_start_lat4 = this%fire_ignition_start_lat4
+      fire_ignition_end_lon4 = this%fire_ignition_end_lon4
+      fire_ignition_end_lat4 = this%fire_ignition_end_lat4
+      fire_ignition_ros4 = this%fire_ignition_ros4
+      fire_ignition_start_time4 = this%fire_ignition_start_time4
+      fire_ignition_end_time4 = this%fire_ignition_end_time4
+      fire_ignition_radius4 = this%fire_ignition_radius4
+
+      fire_ignition_start_lon5 = this%fire_ignition_start_lon5
+      fire_ignition_start_lat5 = this%fire_ignition_start_lat5
+      fire_ignition_end_lon5 = this%fire_ignition_end_lon5
+      fire_ignition_end_lat5 = this%fire_ignition_end_lat5
+      fire_ignition_ros5 = this%fire_ignition_ros5
+      fire_ignition_start_time5 = this%fire_ignition_start_time5
+      fire_ignition_end_time5 = this%fire_ignition_end_time5
+      fire_ignition_radius5 = this%fire_ignition_radius5
 
       open (newunit = unit_nml, file = trim (file_name), action = 'read', iostat = io_stat)
       if (io_stat /= 0) then
